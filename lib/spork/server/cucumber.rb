a03638913602cac170fe0cbe72c2e8cca882dc67
Ben Mabey <ben@benmabey.com>
2009-05-31 04:57:30 +0800

    fixed bugs with Cucumber server WRT step mother and incorrect scoping.
    Signed-off-by: Tim Harper <timcharper@gmail.com>

--- lib/spork/server/cucumber.rb
+++ lib/spork/server/cucumber.rb
@@ -2,22 +2,26 @@ class Spork::Server::Cucumber < Spork::Server
   CUCUMBER_PORT = 8990
   CUCUMBER_HELPER_FILE = File.join(Dir.pwd, "features/support/env.rb")
   
-  def self.port
-    CUCUMBER_PORT
+  class << self
+    def port
+      CUCUMBER_PORT
+    end
+    
+    def helper_file
+      CUCUMBER_HELPER_FILE
+    end
+
+    attr_accessor :step_mother
   end
   
-  def self.helper_file
-    CUCUMBER_HELPER_FILE
-  end
-  
-  def self.step_mother=(value)
-    @step_mother = value
+  def step_mother
+    self.class.step_mother
   end
   
   def run_tests(argv, stderr, stdout)
     require 'cucumber/cli/main'
-    ::Cucumber::Cli::Main.step_mother = @step_mother
-    ::Cucumber::Cli::Main.new(argv, stderr, stdout).execute!(@step_mother)
+    ::Cucumber::Cli::Main.step_mother = step_mother
+    ::Cucumber::Cli::Main.new(argv, stdout, stderr).execute!(step_mother)
   end
 end
 
