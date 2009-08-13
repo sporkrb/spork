84621b1890dddf982963e0446fd4b1e505cf40d6
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2009-08-13 12:31:41 +0200

    Use the same Cucumber instead of some random old cucumber gem

--- features/steps/rails_steps.rb
+++ features/steps/rails_steps.rb
@@ -2,7 +2,7 @@ Given /^I am in a fresh rails project named "(.+)"$/ do |folder_name|
   @current_dir = SporkWorld::SANDBOX_DIR
   version_argument = ENV['RAILS_VERSION'] ? "_#{ENV['RAILS_VERSION']}_" : nil
   # run("#{SporkWorld::RUBY_BINARY} #{%x{which rails}.chomp} #{folder_name}")
-  run([SporkWorld::RUBY_BINARY, %x{which rails}.chomp, version_argument, folder_name].compact * " ")
+  run([SporkWorld::RUBY_BINARY, '-I', Cucumber::LIBDIR, %x{which rails}.chomp, version_argument, folder_name].compact * " ")
   @current_dir = File.join(File.join(SporkWorld::SANDBOX_DIR, folder_name))
 end
 
