afbf621af642dc3624e73d4271c48218e9730d2f
Chris Flipse <cflipse@gmail.com>
2009-08-14 23:52:59 -0400

    $RSPEC_DRB env overrides default rspec port

--- spec/spork/server/rspec_spec.rb
+++ spec/spork/server/rspec_spec.rb
@@ -5,8 +5,18 @@ describe Spork::Server::RSpec do
     @server = Spork::Server::RSpec.new
   end
   
-  it "uses the RSPEC_PORT for it's port" do
+  it "uses the RSPEC_PORT for it's default port" do
     @server.port.should == Spork::Server::RSpec::RSPEC_PORT
+  end
+
+  it "uses ENV['RSPEC_DRB'] as port if present" do
+    orig = ENV['RSPEC_DRB']
+    begin
+      ENV['RSPEC_DRB'] = "9000"
+      @server.port.should == 9000
+    ensure
+      ENV['RSPEC_DRB'] = orig
+    end
   end
   
   it "uses the RSPEC_HELPER_FILE for it's helper_file" do
