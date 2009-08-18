fce52749d10ba1c5c440d6060c617c81446f0c8f
Tim Harper <timcharper@gmail.com>
2009-08-18 10:11:37 -0600

    fix cucumber integration to no longer preload cucumber when cucumber isn't loaded

--- lib/spork/test_framework/cucumber.rb
+++ lib/spork/test_framework/cucumber.rb
@@ -1,27 +1,24 @@
-require 'cucumber'
-
 class Spork::TestFramework::Cucumber < Spork::TestFramework
   DEFAULT_PORT = 8990
   HELPER_FILE = File.join(Dir.pwd, "features/support/env.rb")
 
   class << self
     # REMOVE WHEN SUPPORT FOR 0.3.95 AND EARLIER IS DROPPED
-    attr_accessor :step_mother
+    attr_accessor :mother_object
   end
 
-  # REMOVE WHEN SUPPORT FOR 0.3.95 AND EARLIER IS DROPPED
-  def step_mother
-    self.class.step_mother
+  def preload
+    require 'cucumber'
+    begin
+      @step_mother = ::Cucumber::StepMother.new
+      @step_mother.load_programming_language('rb')
+    rescue NoMethodError => pre_cucumber_0_4 # REMOVE WHEN SUPPORT FOR PRE-0.4 IS DROPPED
+      @step_mother = Spork::Server::Cucumber.mother_object
+    end
+    super
   end
 
   def run_tests(argv, stderr, stdout)
-    ::Cucumber::Cli::Main.new(argv, stdout, stderr).execute!(step_mother)
+    ::Cucumber::Cli::Main.new(argv, stdout, stderr).execute!(@step_mother)
   end
-end
-
-begin
-  Spork::TestFramework::Cucumber.step_mother = ::Cucumber::StepMother.new
-  Spork::TestFramework::Cucumber.step_mother.load_programming_language('rb') if defined?(Spork::TestFramework)
-rescue NoMethodError => pre_cucumber_0_4 # REMOVE WHEN SUPPORT FOR 0.3.95 AND EARLIER IS DROPPED
-  Spork::TestFramework::Cucumber.step_mother = self
 end
