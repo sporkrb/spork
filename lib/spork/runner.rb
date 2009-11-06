sporkrb/spork

commit 0c733e11925257f451f85343c9b5383950e36111

Immediately start listening when firing up spork.
This way, you can start spork and not have to wait for it to boot up
before giving it a request to run specs

lib/spork/runner.rb
@@ -70,7 +70,7 @@ module Spork
         return true
       else
         run_strategy = Spork::RunStrategy.factory(test_framework)
-        return(false) unless run_strategy.preload
+        run_strategy.preload_in_background
         Spork::Server.run(:port => @options[:port] || test_framework.default_port, :run_strategy => run_strategy)
         return true
       end

lib/spork/run_strategy.rb
@@ -4,37 +4,59 @@ class Spork::RunStrategy
 
   def initialize(test_framework)
     @test_framework = test_framework
+    @state = nil
   end
 
   def preload
-    raise NotImplementedError
+    return unless @state == nil
+    @state = :preloading
+    result = do_preload
+    @state = :ready
+    result
   end
 
-  def run(argv, input, output)
-    raise NotImplementedError
+  def run(argv, stderr, stdout)
+    raise RuntimeError, "RunStrategy#preload has not been invoked" if @state == nil
+    sleep 0.1 while @state == :preloading
+    do_run(argv, stderr, stdout)
   end
 
-  def cleanup
-    raise NotImplementedError
+  def preload_in_background
+    Thread.new { preload }
   end
 
   def running?
     raise NotImplementedError
   end
 
+  def self.factory(test_framework)
+    Spork::RunStrategy::Forking.new(test_framework)
+  end
+
   def abort
-    raise NotImplementedError
+    do_abort
   end
 
   protected
-    def self.factory(test_framework)
-      Spork::RunStrategy::Forking.new(test_framework)
+    def do_abort
+      raise NotImplementedError
     end
 
     def self.inherited(subclass)
       @@run_strategies << subclass
     end
 
+    def do_preload
+      raise NotImplementedError
+    end
+
+    def do_run(argv, input, output)
+      raise NotImplementedError
+    end
+
+    def do_cleanup
+      raise NotImplementedError
+    end
 end
 
 Dir[File.dirname(__FILE__) + "/run_strategy/*.rb"].each { |file| require file }

lib/spork/run_strategy/forking.rb
@@ -3,28 +3,29 @@ class Spork::RunStrategy::Forking < Spork::RunStrategy
     Kernel.respond_to?(:fork)
   end
 
-  def run(argv, stderr, stdout)
-    abort if running?
-
-    @child = ::Spork::Forker.new do
-      $stdout, $stderr = stdout, stderr
-      load test_framework.helper_file
-      Spork.exec_each_run
-      test_framework.run_tests(argv, stderr, stdout)
-    end
-    @child.result
-  end
-
-  def abort
-    @child && @child.abort
-  end
-
-  def preload
-    test_framework.preload
-  end
-
   def running?
     @child && @child.running?
   end
+
+  protected
+    def do_run(argv, stderr, stdout)
+      abort if running?
+
+      @child = ::Spork::Forker.new do
+        $stdout, $stderr = stdout, stderr
+        load test_framework.helper_file
+        Spork.exec_each_run
+        test_framework.run_tests(argv, stderr, stdout)
+      end
+      @child.result
+    end
+
+    def do_abort
+      @child && @child.abort
+    end
+
+    def do_preload
+      test_framework.preload
+    end
 
 end

spec/spork/runner_spec.rb
@@ -25,13 +25,6 @@ describe Spork::Runner do
     Spork::Runner.new(['rspec', '-b'], @out, @err).run
   end
   
-  it "aborts if it can't preload" do
-    use_test_server
-    @test_framework.should_receive(:preload).and_return(false)
-    Spork::Server.should_not_receive(:run)
-    Spork::Runner.new(['rspec'], @out, @err).run
-  end
-  
   it "runs the server if all is well" do
     use_test_server
     @test_framework.should_receive(:preload).and_return(true)

commit fb674f4d71c5b365d1ea67b1848a2ea4e5467820

fixes regression introduced by 0c733e119
spork immediately quit with cucumber

lib/spork/runner.rb
@@ -69,6 +69,7 @@ module Spork
         Spork::Diagnoser.output_results(@output)
         return true
       else
+        Spork.using_spork!
         run_strategy = Spork::RunStrategy.factory(test_framework)
         run_strategy.preload_in_background
         Spork::Server.run(:port => @options[:port] || test_framework.default_port, :run_strategy => run_strategy)

lib/spork/server.rb
@@ -22,6 +22,7 @@ class Spork::Server
   
   # Sets up signals and starts the DRb service. If it's successful, it doesn't return. Not ever.  You don't need to override this.
   def listen
+    raise RuntimeError, "you must call Spork.using_spork! before starting the server" unless Spork.using_spork?
     trap("SIGINT") { sig_int_received }
     trap("SIGTERM") { abort; exit!(0) }
     trap("USR2") { abort; restart } if Signal.list.has_key?("USR2")