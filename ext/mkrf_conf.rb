83936a76a0e68259ba4bbd8f14421afc1db46f38
Tim Harper <timcharper@gmail.com>
2010-04-02 15:33:38 -0600

    addresses an issue where spork conditional dependency installation would unnecesarily fail

--- ext/mkrf_conf.rb
+++ ext/mkrf_conf.rb
@@ -2,17 +2,23 @@ require 'rubygems'
 require 'rubygems/command.rb'
 require 'rubygems/dependency_installer.rb' 
 STDERR.puts "Actually, there aren't any native extensions. I'm just dynamically installing dependencies based off of your operating system"
-begin
-  Gem::Command.build_args = ARGV
-  rescue NoMethodError
-end 
 inst = Gem::DependencyInstaller.new
+
+# this will fail if rake isn't installed.
 begin
   inst.install "rake"
-  inst.install "win32-process", "~> 0.6.1" if RUBY_PLATFORM =~ /mswin|mingw/ and RUBY_VERSION < '1.9.1'
 rescue
-  exit(1)
+  # oh well.  Let it fail later.
 end 
+
+if RUBY_PLATFORM =~ /mswin|mingw/ and RUBY_VERSION < '1.9.1'
+  STDERR.puts "installing windows dependencies"
+  begin
+    inst.install "win32-process", "~> 0.6.1"
+  rescue
+    exit(1)
+  end
+end
 
 f = File.open(File.join(File.dirname(__FILE__), "Rakefile"), "w")   # create dummy rakefile to indicate success
 f.write("task :default\n")
