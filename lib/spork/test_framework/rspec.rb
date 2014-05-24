38c79dcedb246daacbadb9f18d09f50cc837de51
Brad Carson <brad@pixelwavedesign.com>
2014-05-24 15:57:57 -0400

    replace CommandLine with Runner for RSpec 3.0.0.rc1

--- lib/spork/test_framework/rspec.rb
+++ lib/spork/test_framework/rspec.rb
@@ -1,3 +1,5 @@
+require 'rspec/core/version'
+
 class Spork::TestFramework::RSpec < Spork::TestFramework
   DEFAULT_PORT = 8989
   HELPER_FILE = File.join(Dir.pwd, "spec/spec_helper.rb")
@@ -7,9 +9,17 @@ class Spork::TestFramework::RSpec < Spork::TestFramework
       ::Spec::Runner::CommandLine.run(
         ::Spec::Runner::OptionParser.parse(argv, stderr, stdout)
       )
+    elsif rspec3?
+      options = ::RSpec::Core::ConfigurationOptions.new(argv)
+      ::RSpec::Core::Runner.new(options).run(stderr, stdout)
     else
       ::RSpec::Core::CommandLine.new(argv).run(stderr, stdout)
     end
+  end
+
+  def rspec3?
+    return false if !defined?(::RSpec::Core::Version::STRING)
+    ::RSpec::Core::Version::STRING =~ /^3\./
   end
 
   def rspec1?
