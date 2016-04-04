e576cc9bff935a1cd8f31dd3ccf33099da2d31a7
Eric Guo <eric.guo@sandisk.com>
2016-04-04 10:37:52 +0800

    Drop rspec1 support.

--- lib/spork/test_framework/rspec.rb
+++ lib/spork/test_framework/rspec.rb
@@ -8,13 +8,8 @@ class Spork::TestFramework::RSpec < Spork::TestFramework
     HELPER_FILE = File.join(Dir.pwd, "spec/spec_helper.rb")
   end
 
-
   def run_tests(argv, stderr, stdout)
-    if rspec1?
-      ::Spec::Runner::CommandLine.run(
-        ::Spec::Runner::OptionParser.parse(argv, stderr, stdout)
-      )
-    elsif rspec3? || rspec2_99?
+    if rspec3? || rspec2_99?
       options = ::RSpec::Core::ConfigurationOptions.new(argv)
       ::RSpec::Core::Runner.new(options).run(stderr, stdout)
     else
@@ -30,9 +25,5 @@ class Spork::TestFramework::RSpec < Spork::TestFramework
   def rspec2_99?
     return false if !defined?(::RSpec::Core::Version::STRING)
     ::RSpec::Core::Version::STRING =~ /^2\.99\./
-  end
-
-  def rspec1?
-    defined?(Spec) && !defined?(RSpec)
   end
 end
